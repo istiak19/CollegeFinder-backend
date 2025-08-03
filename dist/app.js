c839d7b1ea23512743ec246007cbb6f4fd80b7cf
istiak19 <istiak15-14128@diu.edu.bd>
2025-08-03 07:36:15 +0600

    college route created

--- dist/app.js
+++ dist/app.js
@@ -6,17 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const user_route_1 = require("./app/user/user.route");
+const college_route_1 = require("./app/college/college.route");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
     origin: [
         "http://localhost:5173",
+        "https://collegefinder19.netlify.app"
     ],
     credentials: true
 }));
 app.use("/api/v1/", user_route_1.userRoutes);
-// app.use("/api", borrowRoute);
+app.use("/api/v1", college_route_1.collegeRoutes);
 app.get("/", (req, res) => {
     res.send("College finder API is running successfully!");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { userRoutes } from "./app/user/user.route";
+import { collegeRoutes } from "./app/college/college.route";
 
 const app: Application = express();
 
@@ -9,12 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
         "http://localhost:5173",
+        "https://collegefinder19.netlify.app"
     ],
     credentials: true
 }));
 
 app.use("/api/v1/", userRoutes);
-// app.use("/api", borrowRoute);
+app.use("/api/v1", collegeRoutes);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("College finder API is running successfully!");
